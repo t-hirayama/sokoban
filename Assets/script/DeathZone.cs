f19647e008e2b3c1a4b0372564287bc58f23cdf7
t-hirayama <hirayama606@gmail.com>
2015-03-16 01:05:49 +0900

    put pushable box.

---
+++ Assets/script/DeathZone.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+	public void OnTriggerEnter( Collider col ) {
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
---
+++ Assets/script/Surface.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Surface : MonoBehaviour {
+
+	public void OnTriggerEnter( Collider col ) {
+		const float REF_VALUE = 0.5f;
+		const float DISTANCE = 2.0f;
+
+		if (transform.localPosition.x > REF_VALUE) {
+			transform.parent.Translate(new Vector3(-DISTANCE, 0.0f, 0.0f));
+		}
+		else if (transform.localPosition.x < -REF_VALUE) {
+			transform.parent.Translate(new Vector3(DISTANCE, 0.0f, 0.0f));
+		}
+		else if (transform.localPosition.z > REF_VALUE) {
+			transform.parent.Translate(new Vector3(0.0f, 0.0f, -DISTANCE));
+		}
+		else if (transform.localPosition.z < -REF_VALUE) {
+			transform.parent.Translate(new Vector3(0.0f, 0.0f, DISTANCE));
+		}
+		/*
+*/		
+	}
+}

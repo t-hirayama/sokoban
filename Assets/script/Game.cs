8f6cf9c8fa3980b31e0fdc07568f044963db866e
t-hirayama <hirayama606@gmail.com>
2015-03-16 13:13:22 +0900

    add some scripts.

---
+++ Assets/script/Game.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game {
+
+	private static Stage stage;
+
+	public static void initialize() {
+		stage = new Stage();
+	}
+
+	public static Stage getStage() {
+		return stage;
+	}
+
+
+}
---
+++ Assets/script/Startup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Game.initialize ();
+
+		Stage s = Game.getStage ();
+
+
+
+
+		Debug.Log ("startup " + s.val);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
